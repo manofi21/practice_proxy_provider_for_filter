98d8f9c370cd0ec54f612e34df4d65e02a75f323
Jeremia Manogi Mario <jeremimanogi@gmail.com>
2023-05-31 20:20:40 +0700

    create theme value

---
+++ proxy_provider_for_filter/lib/theme/theme_value.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const double sizeS = 10;
+const double sizeMS = 15;
+const double sizeM = 20;
+const double sizeML = 25;
+const double borderRadiusValue = 8;
+const double fieldHeight = 45;
+const EdgeInsets formDefaultPadding = EdgeInsets.symmetric(
+  horizontal: 15,
+  vertical: 11,
+);

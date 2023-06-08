51fe3dfa9bf75afaacd08fb98256521dcf6d254f
unknown <jeremimanogi@gmail.com>
2023-06-08 18:00:52 +0700

    create BaseDropdownReturn

---
+++ lib/entities/base_dropdown_return.dart
@@ -0,0 +1,8 @@
+import 'dropdown_item.dart';
+
+class BaseDropdownReturn{
+  final List<DropdownItem> listType;
+  final List<DropdownItem> listStatus;
+
+  BaseDropdownReturn({required this.listType,required  this.listStatus});
+}

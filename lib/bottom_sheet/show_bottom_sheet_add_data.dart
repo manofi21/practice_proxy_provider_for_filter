ef3a948b39bcc0678487a20455665c310fa1fa4c
unknown <jeremimanogi@gmail.com>
2023-06-08 13:11:22 +0700

    create show_bottom_sheet_add_data

---
+++ lib/bottom_sheet/show_bottom_sheet_add_data.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:proxy_provider_for_filter/provider/watching_provider.dart';
+
+import '../provider/gaming_provider.dart';
+import '../provider/reading_provider.dart';
+import '../widget/add_data_dialog.dart';
+import '../widget/base_widget/bottom_sheet.dart';
+
+Future<void> showBottomSheetAddData(
+  BuildContext context, {
+  required int index,
+}) async {
+  late Widget dialogWidget;
+  if (index == 0) {
+    dialogWidget = addDataDialog<WatchingProvider>(
+      context,
+      listStatus: [],
+      listType: [],
+    );
+  }
+
+  if (index == 1) {
+    dialogWidget = addDataDialog<ReadingProvider>(
+      context,
+      listStatus: [],
+      listType: [],
+    );
+  }
+
+  if (index == 2) {
+    dialogWidget = addDataDialog<GamingProvider>(
+      context,
+      listStatus: [],
+      listType: [],
+    );
+  }
+
+  showBaseBottomSheet(
+    currentcontext: context,
+    padding: const EdgeInsets.all(0),
+    builder: (context) {
+      return dialogWidget;
+    },
+  );
+}

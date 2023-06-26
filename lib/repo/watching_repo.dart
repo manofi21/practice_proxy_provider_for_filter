a0456f1ced18c7c853fd4db20f52eaa5c2a4d541
unknown <jeremimanogi@gmail.com>
2023-06-26 21:45:00 +0700

    add getListWatchingHistory in watchin repo

--- lib/repo/watching_repo.dart
+++ lib/repo/watching_repo.dart
@@ -1,28 +1,44 @@
 import 'package:proxy_provider_for_filter/model/watch_status_model_v1.dart';
 
 import '../entities/watching_model_entity.dart';
+import '../model/filter_model/filter_history_model.dart';
 import '../model/watch_type_model_v1.dart';
 import '../model/watching_model_v1.dart';
 import '../sql_db/sql_repo.dart';
-import '../utils/convert_maps_to_entity.dart' show convertMapToWatchingEntitiy, keyidtable;
+import '../utils/convert_maps_to_entity.dart'
+    show convertMapToWatchingEntitiy, keyidtable;
 
 abstract class WatchingRepo {
   Future<List<WatchTypeModelV1>> getListWatchTypeModel();
   Future<List<WatchStatusModelV1>> getListWatcStatusModel();
-  Future<List<WatchingModelEntity>> getListWatchingHistory();
+  Future<List<WatchingModelEntity>> getListWatchingHistory(
+      {FilterHistoryModel? filterHistoryModel});
   Future<void> addWatchingHistory(WatchingModelV1 watchingEntity);
   Future<void> updateWatchingHistory(WatchingModelV1 watchingEntity);
 }
 
 class WatchingRepoImpl implements WatchingRepo {
   @override
-  Future<List<WatchingModelEntity>> getListWatchingHistory() async {
+  Future<List<WatchingModelEntity>> getListWatchingHistory(
+      {FilterHistoryModel? filterHistoryModel}) async {
     final sqlBase = SqlBaseRepoImpl();
-    final resultFromQuery = await sqlBase.getListDataRawQuery<WatchingModelEntity>(
+    final filterModel = filterHistoryModel?.toStringFilterQUery(
+      WatchingModelV1.table,
+      columnStatusId: WatchingModelV1.keyidstatus,
+      columnFavorite: WatchingModelV1.keyfavorite,
+    );
+    final whereQuery = filterModel?.whereQuery ?? '';
+    final whereArgument = filterModel?.argument ?? [];
+
+    final resultFromQuery =
+        await sqlBase.getListDataRawQuery<WatchingModelEntity>(
       fromMap: convertMapToWatchingEntitiy,
-      rawQuery: "SELECT ${WatchingModelV1.table}.${WatchingModelV1.keyid} as $keyidtable,* FROM ${WatchingModelV1.table} "
+      rawQuery:
+          "SELECT ${WatchingModelV1.table}.${WatchingModelV1.keyid} as $keyidtable,* FROM ${WatchingModelV1.table} "
           "INNER JOIN ${WatchTypeModelV1.table} ON ${WatchingModelV1.table}.${WatchingModelV1.keyidtype} = ${WatchTypeModelV1.table}.${WatchTypeModelV1.keyid} "
-          "INNER JOIN ${WatchStatusModelV1.table} ON ${WatchingModelV1.table}.${WatchingModelV1.keyidstatus} = ${WatchStatusModelV1.table}.${WatchStatusModelV1.keyid}",
+          "INNER JOIN ${WatchStatusModelV1.table} ON ${WatchingModelV1.table}.${WatchingModelV1.keyidstatus} = ${WatchStatusModelV1.table}.${WatchStatusModelV1.keyid}"
+          "$whereQuery",
+      arguments: whereArgument,
     );
 
     return resultFromQuery;
@@ -37,18 +53,25 @@ class WatchingRepoImpl implements WatchingRepo {
   @override
   Future<void> updateWatchingHistory(WatchingModelV1 watchingEntity) async {
     final sqlBase = SqlBaseRepoImpl();
-    await sqlBase.insertData(WatchingModelV1.table, watchingEntity.toMap());
+    await sqlBase.updateData(
+      WatchingModelV1.table,
+      watchingEntity.toUpdateMap(),
+      where: "${WatchingModelV1.keyid} = ?",
+      whereArgs: [watchingEntity.id],
+    );
   }
-  
+
   @override
   Future<List<WatchTypeModelV1>> getListWatchTypeModel() {
     final sqlBase = SqlBaseRepoImpl();
-    return sqlBase.getListData(table: WatchTypeModelV1.table, fromMap: WatchTypeModelV1.fromMap);
+    return sqlBase.getListData(
+        table: WatchTypeModelV1.table, fromMap: WatchTypeModelV1.fromMap);
   }
 
   @override
   Future<List<WatchStatusModelV1>> getListWatcStatusModel() {
     final sqlBase = SqlBaseRepoImpl();
-    return sqlBase.getListData(table: WatchStatusModelV1.table, fromMap: WatchStatusModelV1.fromMap);
+    return sqlBase.getListData(
+        table: WatchStatusModelV1.table, fromMap: WatchStatusModelV1.fromMap);
   }
 }

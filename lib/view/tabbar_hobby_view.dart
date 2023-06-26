3e83b38857ced6207cb69e856dade37397c499ee
unknown <jeremimanogi@gmail.com>
2023-06-26 18:56:56 +0700

    change use didChangeDependencies and impl circular indicator

--- lib/view/tabbar_hobby_view.dart
+++ lib/view/tabbar_hobby_view.dart
@@ -1,37 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:proxy_provider_for_filter/utils/on_update_favorite_entity.dart';
 import 'package:proxy_provider_for_filter/widget/filter_dialog.dart';
 
 import '../provider/base_provider.dart';
 import '../utils/get_name_of_title.dart';
 import '../widget/tile_list_card_widget.dart';
 
-class TabbarHobbyView<FP extends BaseProvider> extends StatefulWidget {
+class TabbarHobbyView<BP extends BaseProvider> extends StatefulWidget {
   const TabbarHobbyView({Key? key}) : super(key: key);
 
   @override
-  State<TabbarHobbyView<FP>> createState() => _TabbarHobbyViewState<FP>();
+  State<TabbarHobbyView<BP>> createState() => _TabbarHobbyViewState<BP>();
 }
 
-class _TabbarHobbyViewState<FP extends BaseProvider>
-    extends State<TabbarHobbyView<FP>> {
+class _TabbarHobbyViewState<BP extends BaseProvider>
+    extends State<TabbarHobbyView<BP>> {
+
   @override
-  void initState() {
-    super.initState();
+  void didChangeDependencies() {
+    final _prov = context.read<BP>();
+
+    // For handle notifyListeners in initData.
     WidgetsBinding.instance.addPostFrameCallback((duration) async {
-      final _prov = context.read<FP>();
-      await _prov.initData(context);
+      _prov.initData(context);
+      super.didChangeDependencies();
     });
   }
 
   @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Consumer<FP>(
+    return Consumer<BP>(
       builder: (context, value, child) {
+        if (value.loading) {
+          return const SizedBox(
+            height: 50,
+            width: 50,
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+
         return SingleChildScrollView(
           child: Column(
             children: value.getListValue
@@ -39,6 +48,14 @@ class _TabbarHobbyViewState<FP extends BaseProvider>
                   (e) => TileListCardWidget(
                     nameOfTitle: getNameOfTitle(value),
                     baseModelEntity: e,
+                    onFavorite: () async {
+                      final changeFavValue = onUpdateFavoriteEntity<BP>(
+                        inputModel: e,
+                      );
+                      if (changeFavValue != null) {
+                        await value.updateData(context, changeFavValue);
+                      }
+                    },
                   ),
                 )
                 .toList(),
